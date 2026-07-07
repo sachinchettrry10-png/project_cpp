23862b51aa1b884f76ab7514859b76a33ad9ca2c
sachinchettrry10-png <sachinchettrry10@gmail.com>
2026-07-07 08:15:01 +0545

    Create demo.cpp

---
+++ demo.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello World!" << endl;
+
+    cout << "Name: John" << endl;
+    cout << "Age: 20" << endl;
+    cout << "Hobby: Football" << endl;
+
+    return 0;
+}
